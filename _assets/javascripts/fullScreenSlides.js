ada770766bd7c21539c1a0078e5c630657d441b9
James Cole <jcole@zivtech.com>
2015-04-13 10:54:10 -0400

    integrate slide scrolling actions

---
+++ _assets/javascripts/fullScreenSlides.js
@@ -0,0 +1,82 @@
+var Proviso = (function (parent, $) {
+
+  var fullScreenSlides = parent.fullScreenSlides = parent.fullScreenSlides || {};
+
+  fullScreenSlides.settings = $.extend({}, {
+    slides: $('.segment.slide'),
+    nav: $('.slide-nav a'),
+    useModal: true,
+    modalSelector: '#mc_embed_signup'
+  });
+
+  fullScreenSlides.setActiveNav = function(slide) {
+    // remove active class from all the other toggles
+    fullScreenSlides.settings.nav.removeClass('active');
+
+    // add an active class to it
+    $(fullScreenSlides.settings.nav[slide]).addClass('active');
+  };
+
+  fullScreenSlides.scrollToSlide = function(slide) {
+    // scroll to slide
+    $('html, body').animate({
+      scrollTop: $(fullScreenSlides.settings.slides[slide]).offset().top
+    }, 1000);
+  };
+
+  fullScreenSlides.setupSlideToggles = function() {
+    fullScreenSlides.settings.nav.each(function(i) {
+      // cache this
+      var toggle = $(this);
+
+      // set the click handling
+      toggle.on('click', function (e) {
+        e.preventDefault();
+
+        // find the slide this link is controlling
+        var slide = toggle.attr('data-slide-href');
+
+        // view the slide
+        fullScreenSlides.scrollToSlide(slide);
+
+        // set the active slide
+        fullScreenSlides.setActiveNav(slide);
+      });
+    });
+  };
+
+  fullScreenSlides.setupScrollSlides = function() {
+    $(window).on('touchmove wheel', function(e) {
+      e.handled = false;
+
+      if (!e.handled) {
+        var min = 1000
+          , activeSlide = $('.slide-nav .active').attr('data-slide-href')
+          , $inView = $(fullScreenSlides.settings.slides[activeSlide]);
+
+        fullScreenSlides.settings.slides.each(function (k) {
+          var $slide = $(fullScreenSlides.settings.slides[k]);
+          var position = Math.abs($slide.offset().top - $(window).scrollTop());
+
+          if (position < min) {
+            min = position;
+            $inView = $slide;
+          }
+        });
+
+        activeSlide = $inView.attr('data-slide');
+        fullScreenSlides.setActiveNav(activeSlide);
+        e.handled = true;
+      }
+    });
+  };
+
+  if (fullScreenSlides.settings.slides.length
+    && fullScreenSlides.settings.nav.length) {
+    fullScreenSlides.setupSlideToggles();
+    fullScreenSlides.setupScrollSlides();
+  }
+
+  return parent;
+
+})(Proviso || {}, jQuery);
--- _assets/javascripts/proviso.js
+++ _assets/javascripts/proviso.js
@@ -1,5 +1,8 @@
 //= require modernizr
+//= require ga
+//= require jquery-2.1.3.min
+//= require fullScreenSlides
 
-(function() {
+(function($) {
 
-})();
+})(jQuery);

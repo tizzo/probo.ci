9ffae19cb68c266e3e14f03c1693a0241e6f9620
James Cole <jcole@zivtech.com>
2015-09-08 10:31:09 -0400

    execute gh-pages deletion script only if not a PR

--- delete-gh-pages.sh
+++ delete-gh-pages.sh
@@ -2,10 +2,15 @@
 # 
 # Delete gh-pages branch before travis rebuilds site to remove caches
 
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
 set -e
 
 git config --local user.name "Travis CI"
-git config --local user.email "travis@travis-ci.org"
+git config --local user.email "jcole@zivtech.com"
 git clone -q https://${GH_TOKEN}@github.com/ProboCI/probo.ci.git to_delete > /dev/null 2>&1
 cd to_delete
 git checkout gh-pages
